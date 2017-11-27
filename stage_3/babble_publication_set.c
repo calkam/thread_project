00faafdbc541afcfd7bf4fae8db5ee2ad984ae55
calkam <calka.maxime@hotmail.com>
2017-11-27 14:41:12 +0100

    modif

--- stage_3/babble_publication_set.c
+++ stage_3/babble_publication_set.c
@@ -2,9 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 
 #include "babble_publication_set.h"
 #include "babble_server.h"
+
+int readerWaiting_pub = 0;
+int readerCount_pub = 0;
+int writerWaiting_pub = 0;
+int writer_pub = 0;
+pthread_mutex_t mutex_pub = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_read_pub = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_write_pub = PTHREAD_COND_INITIALIZER;
+
+void startWrite_pub(void){
+    pthread_mutex_lock(&mutex_pub);
+    writerWaiting_pub = writerWaiting_pub + 1;
+    while((readerCount_pub > 0) || (writer_pub == 1)){
+        pthread_cond_wait(&cond_write_pub, &mutex_pub);
+    }
+    writerWaiting_pub = writerWaiting_pub - 1;
+    writer_pub = 1;
+    pthread_mutex_unlock(&mutex_pub);
+}
+
+void endWrite_pub(void){
+    pthread_mutex_lock(&mutex_pub);
+    writer_pub = 0;
+    if(writerWaiting_pub > 0){
+        pthread_cond_signal(&cond_write_pub);
+    }else if(readerWaiting_pub > 0){
+        pthread_cond_broadcast(&cond_read_pub);
+    }
+    pthread_mutex_unlock(&mutex_pub);
+}
+
+void startRead_pub(void){
+    pthread_mutex_lock(&mutex_pub);
+    readerWaiting_pub = readerWaiting_pub + 1;
+    while((writerWaiting_pub > 0) || (writer_pub == 1)){
+        pthread_cond_wait(&cond_read_pub, &mutex_pub);
+    }
+    readerWaiting_pub = readerWaiting_pub - 1;
+    readerCount_pub = readerCount_pub + 1;
+    pthread_mutex_unlock(&mutex_pub);
+}
+
+void endRead_pub(void){
+    pthread_mutex_lock(&mutex_pub);
+    readerCount_pub = readerCount_pub - 1;
+    if((readerCount_pub == 0) && (writerWaiting_pub > 0)){
+        pthread_cond_signal(&cond_write_pub);
+    }
+    pthread_mutex_unlock(&mutex_pub);
+}
 
 publication_set_t* publication_set_create(void)
 {
@@ -18,6 +69,8 @@ publication_set_t* publication_set_create(void)
 
 publication_t* publication_set_insert(publication_set_t *set, char* msg)
 {
+    startWrite_pub();
+    
     struct timespec tt;
     
     publication_t *pub= malloc(sizeof(publication_t));
@@ -40,12 +93,16 @@ publication_t* publication_set_insert(publication_set_t *set, char* msg)
         set->last = pub;
     }
     
+    endWrite_pub();
+    
     return pub;
 }
 
-
 publication_t* publication_set_getnext(publication_set_t *set, publication_t* last_pub, uint64_t min_date)
 {   
+
+	startRead_pub();
+
     if(last_pub != NULL){
         if(last_pub->next == NULL || last_pub->next->ndate >= min_date){
             return last_pub->next;
@@ -58,6 +115,7 @@ publication_t* publication_set_getnext(publication_set_t *set, publication_t* la
         item = item -> next;
     }
     
+	endRead_pub();
 
     return item;
 }
